c9596d1b0ffb0195fcda296ee513d12704389d39
Touseef Ahmed <84684496+rainyjoke@users.noreply.github.com>
2023-12-12 00:41:15 +0400

    bot fixed- works fine as x but not as o though

--- app/src/main/java/com/example/tictactoe/Bot.java
+++ app/src/main/java/com/example/tictactoe/Bot.java
@@ -2,50 +2,38 @@ package com.example.tictactoe;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.tictactoe.databinding.ActivityMainBinding;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class Bot extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-    private final List<int[]> winning_combos = new ArrayList<>();
-    private int[] positions = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    private int activePlayer = 1;
-    private int totalBoxesSelected = 1;
-    private Boolean endGame = false;
+    private int[][] board = new int[3][3];
+    private int activePlayer;
+    private int human = 1;
+    private int ai = 2;
+    private final int[] pruner = {0, 10, -10};
+    private boolean endGame = false;
     String nameX, nameO;
     int scoreX = 0;
     int scoreO = 0;
     ImageView image;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        winning_combos.add(new int[]{0, 1, 2});
-        winning_combos.add(new int[]{3, 4, 5});
-        winning_combos.add(new int[]{6, 7, 8});
-        winning_combos.add(new int[]{0, 3, 6});
-        winning_combos.add(new int[]{1, 4, 7});
-        winning_combos.add(new int[]{2, 5, 8});
-        winning_combos.add(new int[]{0, 4, 8});
-        winning_combos.add(new int[]{2, 4, 6});
+        setContentView(R.layout.activity_main);
 
         nameX = getIntent().getStringExtra("namePlayerX");
         nameO = getIntent().getStringExtra("namePlayerO");
+        back = findViewById(R.id.back);
+        back.setOnClickListener(v -> finish());
+        initializeGame();
 
-        binding.x.setText(nameX);
-        binding.o.setText(nameO);
-        binding.back.setOnClickListener(v -> finish());
         if (nameX.equals("Bot")) {
             turnCPU();
         } else if (nameX.equals("You")) {
@@ -53,211 +41,299 @@ public class Bot extends AppCompatActivity {
         }
     }
 
-    // method to check if a cell is empty
-    public Boolean isCellEmpty(int position) {
-        return positions[position] == 0;
+    private void initializeGame() {
+        // Additional initialization code, if any...
+        if (!nameX.equals("You")){
+            ai = 1;
+            human = 2;
+            activePlayer = ai;
+        }else{
+            activePlayer = human;
+        }
+
+        ((TextView) findViewById(R.id.x)).setText(nameX);
+        ((TextView) findViewById(R.id.o)).setText(nameO);
+
+    }
+
+    private void setClickListener(int position, ImageView imageView) {
+        imageView.setOnClickListener(view -> {
+            int row = position / 3;
+            int col = position % 3;
+
+            if (isCellEmpty(row, col) && !endGame) {
+                setInput(imageView, row, col);
+                turnCPU();
+            }
+        });
     }
 
     public void turnHuman() {
-        binding.image1.setOnClickListener(view -> {
-            if (isCellEmpty(0)) {
-                setInput((ImageView) view, 0);
-                turnCPU();
+        for (int i = 0; i < 9; i++) {
+            ImageView imageView = getImageByIndex(i / 3, i % 3);
+            if (imageView != null) {
+                setClickListener(i, imageView);
             }
-        });
-        binding.image2.setOnClickListener(view -> {
-            if (isCellEmpty(1)) {
-                setInput((ImageView) view, 1);
-                turnCPU();
-            }
-        });
-        binding.image3.setOnClickListener(view -> {
-            if (isCellEmpty(2)) {
-                setInput((ImageView) view, 2);
-                turnCPU();
-            }
-        });
-        binding.image4.setOnClickListener(view -> {
-            if (isCellEmpty(3)) {
-                setInput((ImageView) view, 3);
-                turnCPU();
-            }
-        });
-        binding.image5.setOnClickListener(view -> {
-            if (isCellEmpty(4)) {
-                setInput((ImageView) view, 4);
-                turnCPU();
-            }
-        });
-        binding.image6.setOnClickListener(view -> {
-            if (isCellEmpty(5)) {
-                setInput((ImageView) view, 5);
-                turnCPU();
-            }
-        });
-        binding.image7.setOnClickListener(view -> {
-            if (isCellEmpty(6)) {
-                setInput((ImageView) view, 6);
-                turnCPU();
-            }
-        });
-        binding.image8.setOnClickListener(view -> {
-            if (isCellEmpty(7)) {
-                setInput((ImageView) view, 7);
-                turnCPU();
-            }
-        });
-        binding.image9.setOnClickListener(view -> {
-            if (isCellEmpty(8)) {
-                setInput((ImageView) view, 8);
-                turnCPU();
-            }
-        });
+        }
+    }
 
+    private void disableHumanInput() {
+        for (int i = 0; i < 9; i++) {
+            ImageView imageView = getImageByIndex(i / 3, i % 3);
+            if (imageView != null) {
+                imageView.setOnClickListener(null);
+            }
+        }
+    }
+
+    private void enableHumanInput() {
+        for (int i = 0; i < 9; i++) {
+            ImageView imageView = getImageByIndex(i / 3, i % 3);
+            if (imageView != null) {
+                setClickListener(i, imageView);
+            }
+        }
     }
 
     public void turnCPU() {
         if (!endGame) {
-            List<Integer> indexes = new ArrayList<>();
+            disableHumanInput();
 
-            for (int i = 0; i < 9; i++) {
-                if (isCellEmpty(i)) {
-                    indexes.add(i);
+            new Handler().postDelayed(() -> {
+                int[] bestMove = findBestMove();
+                image = getImageByIndex(bestMove[0], bestMove[1]);
+                setInput(image, bestMove[0], bestMove[1]);
+
+                enableHumanInput();
+            }, 300);
+        }
+    }
+
+    private int[] findBestMove() {
+        int bestScore = Integer.MIN_VALUE;
+        int[] bestMove = {-1, -1};
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (isCellEmpty(i, j)) {
+                    board[i][j] = ai;
+                    int score = minimax(board, 0, false);
+                    board[i][j] = 0;
+
+                    if (score > bestScore) {
+                        bestScore = score;
+                        bestMove[0] = i;
+                        bestMove[1] = j;
+                    }
                 }
             }
-            Random random = new Random();
-            int randomIndex = indexes.get(random.nextInt(indexes.size()));
-            if (randomIndex == 0) {
-                image = findViewById(R.id.image1);
-            } else if (randomIndex == 1) {
-                image = findViewById(R.id.image2);
-            } else if (randomIndex == 2) {
-                image = findViewById(R.id.image3);
-            } else if (randomIndex == 3) {
-                image = findViewById(R.id.image4);
-            } else if (randomIndex == 4) {
-                image = findViewById(R.id.image5);
-            } else if (randomIndex == 5) {
-                image = findViewById(R.id.image6);
-            } else if (randomIndex == 6) {
-                image = findViewById(R.id.image7);
-            } else if (randomIndex == 7) {
-                image = findViewById(R.id.image8);
-            } else if (randomIndex == 8) {
-                image = findViewById(R.id.image9);
-            }
-            setInput(image, randomIndex);
         }
-        turnHuman();
+        return bestMove;
+    }
+
+    private int minimax(int[][] board, int depth, boolean isMaximizing) {
+        int result = checkWinner();
+        if (result!=-1){
+            return pruner[result];
+        }
+
+        int bestScore = (isMaximizing) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (isCellEmpty(i, j)) {
+                    board[i][j] = (isMaximizing) ? ai : human;
+                    int score = minimax(board, depth + 1, !isMaximizing);
+                    board[i][j] = 0;
+
+                    if ((isMaximizing && score > bestScore) || (!isMaximizing && score < bestScore)) {
+                        bestScore = score;
+                    }
+                }
+            }
+        }
+
+        return bestScore;
+    }
+
+    public int checkWinner() {
+        int winner = -1;
+
+        // Horizontal
+        for (int i = 0; i < 3; i++) {
+            if (equals3(board[i][0], board[i][1], board[i][2])) {
+                winner = board[i][0];
+            }
+        }
+
+        // Vertical
+        for (int i = 0; i < 3; i++) {
+            if (equals3(board[0][i], board[1][i], board[2][i])) {
+                winner = board[0][i];
+            }
+        }
+
+        // Diagonal
+        if (equals3(board[0][0], board[1][1], board[2][2])) {
+            winner = board[0][0];
+        }
+        if (equals3(board[2][0], board[1][1], board[0][2])) {
+            winner = board[2][0];
+        }
+
+
+        if (winner == -1 && isBoardFull()) {
+            return 0;
+        } else {
+            return winner;
+        }
+    }
+
+    private boolean equals3(int a, int b, int c) {
+        return a==b & b==c & a!=0;
     }
 
 
-    // set the input according to the player.
-    public void setInput(ImageView image, int position) {
+    private ImageView getImageByIndex(int row, int col) {
+        switch (row * 3 + col) {
+            case 0:
+                return findViewById(R.id.image1);
+            case 1:
+                return findViewById(R.id.image2);
+            case 2:
+                return findViewById(R.id.image3);
+            case 3:
+                return findViewById(R.id.image4);
+            case 4:
+                return findViewById(R.id.image5);
+            case 5:
+                return findViewById(R.id.image6);
+            case 6:
+                return findViewById(R.id.image7);
+            case 7:
+                return findViewById(R.id.image8);
+            case 8:
+                return findViewById(R.id.image9);
+            default:
+                return null;
+        }
+    }
 
-        positions[position] = activePlayer; // mark the position the player selected
-
+    public void setInput(ImageView image, int row, int col) {
+        board[row][col] = activePlayer;
+        int result = checkWinner();
         if (activePlayer == 1) {
             image.setImageResource(R.drawable.ximage);
-            if (result()) {
+            if (result==1) {
                 endGame = true;
-                displayWinner("win", nameX);
-            } else if (totalBoxesSelected == 9) {
+                scoreX++;
+                displayWinner("win");
+            } else if (result == 0) {
                 endGame = true;
-                displayWinner("draw", "");
+                displayWinner("draw");
             } else {
                 changeTurn(2);
-                totalBoxesSelected++;
             }
         } else {
             image.setImageResource(R.drawable.oimage);
-            if (result()) {
+            if (result==2) {
                 endGame = true;
-                displayWinner("win", nameO);
-            } else if (totalBoxesSelected == 9) {
+                scoreO++;
+                displayWinner("win");
+            } else if (result == 0) {
                 endGame = true;
-                displayWinner("draw", "");
+                displayWinner("draw");
             } else {
                 changeTurn(1);
-                totalBoxesSelected++;
             }
         }
     }
 
-    // display the result
-    @SuppressLint("SetTextI18n")
-    public void displayWinner(String msg, String player) {
-        String win_message;
-        String draw_message = "Match draw.";
+
+    public void displayWinner(String msg) {
+        String winMessage;
+        String drawMessage = "Match draw.";
         ResultBot result;
+
         if (msg.equals("win")) {
-            if (player.equals("You")) {
-                win_message = "Congratulations! You won.";
+            if (activePlayer == human) {
+                winMessage = "Congratulations! You won.";
             } else {
-                win_message = "You lost.";
+                winMessage = "You lost.";
             }
-            result = new ResultBot(Bot.this, win_message, Bot.this);
-            binding.xScore.setText("x: " + scoreX);
-            binding.oScore.setText("o: " + scoreO);
+            result = new ResultBot(Bot.this, winMessage, Bot.this);
+            updateScores(); // Update score text...
         } else {
-            result = new ResultBot(Bot.this, draw_message, Bot.this);
+            result = new ResultBot(Bot.this, drawMessage, Bot.this);
         }
         result.setCancelable(false);
         result.show();
     }
 
-    // method to change player turn
     public void changeTurn(int next) {
-        activePlayer = next; // change and set to next player's turn
+        activePlayer = next;
+        updateUI();
+    }
+    private void updateUI() {
         if (activePlayer == 1) {
-            binding.layoutX.setBackgroundResource(R.drawable.cyan_border);
-            binding.layoutO.setBackgroundResource(R.drawable.white_border);
+            findViewById(R.id.layoutX).setBackgroundResource(R.drawable.cyan_border);
+            findViewById(R.id.layoutO).setBackgroundResource(R.drawable.white_border);
         } else {
-            binding.layoutX.setBackgroundResource(R.drawable.white_border);
-            binding.layoutO.setBackgroundResource(R.drawable.cyan_border);
+            findViewById(R.id.layoutX).setBackgroundResource(R.drawable.white_border);
+            findViewById(R.id.layoutO).setBackgroundResource(R.drawable.cyan_border);
         }
     }
 
-    // method to check if there is a winning combination
-    public Boolean result() {
-        int i = 0;
-        while (i < winning_combos.size()) {
-            final int[] combo = winning_combos.get(i);
-            if (positions[combo[0]] == activePlayer && positions[combo[1]] == activePlayer &&
-                    positions[combo[2]] == activePlayer) {
-                if (activePlayer == 1) {
-                    scoreX++;
-                } else if (activePlayer == 2) {
-                    scoreO++;
-                }
-                return true;
-            }
-            i++;
-        }
-        return false;
-    }
-
-    // restart game method
     public void restartGame() {
-        positions = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+        board = new int[3][3];
         activePlayer = 1;
         endGame = false;
-        totalBoxesSelected = 1;
 
-        binding.image1.setImageResource(0);
-        binding.image2.setImageResource(0);
-        binding.image3.setImageResource(0);
-        binding.image4.setImageResource(0);
-        binding.image5.setImageResource(0);
-        binding.image6.setImageResource(0);
-        binding.image7.setImageResource(0);
-        binding.image8.setImageResource(0);
-        binding.image9.setImageResource(0);
+        resetImages(); // Reset image resources...
 
         if (nameX.equals("Bot")) {
             turnCPU();
         } else if (nameX.equals("You")) {
             turnHuman();
         }
+    }
+
+    private void resetImages() {
+        // Reset the images to default state...
+
+        ((ImageView) findViewById(R.id.image1)).setImageResource(0);
+        ((ImageView) findViewById(R.id.image2)).setImageResource(0);
+        ((ImageView) findViewById(R.id.image3)).setImageResource(0);
+        ((ImageView) findViewById(R.id.image4)).setImageResource(0);
+        ((ImageView) findViewById(R.id.image5)).setImageResource(0);
+        ((ImageView) findViewById(R.id.image6)).setImageResource(0);
+        ((ImageView) findViewById(R.id.image7)).setImageResource(0);
+        ((ImageView) findViewById(R.id.image8)).setImageResource(0);
+        ((ImageView) findViewById(R.id.image9)).setImageResource(0);
+
+    }
+
+    private boolean isCellEmpty(int row, int col) {
+        return board[row][col] == 0;
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (isCellEmpty(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void updateScores() {
+        // Update the score text...
+        TextView xScore = findViewById(R.id.xScore);
+        TextView oScore = findViewById(R.id.oScore);
+        xScore.setText("x: " + scoreX);
+        oScore.setText("o: " + scoreO);
     }
 }
